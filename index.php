baaaa168d79f6a59709543c68b3d65572b984b56
Gen1972 <genaklim1972@mail.ru>
2025-02-04 22:31:48 +0200

    Создал файл для объединения частей сайта

---
+++ index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="ru">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Мой сайт</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <?php include 'header.html'; ?>
+    <?php include 'main.html'; ?>
+    <?php include 'footer.html'; ?>
+</body>
+</html>
